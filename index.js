ca9df046a902826cc2f1f92c2e6e65368f6bb7cd
Natasha Bartley <+>
2023-03-17 09:11:28 -0400

    commit tree 20ee5335b6b884ac02678224813ddd29e555a12c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
