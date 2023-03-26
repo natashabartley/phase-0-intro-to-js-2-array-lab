429490a5bb29ce4cee5d070bd34b5366832117b8
Natasha Bartley <+>
2023-03-26 05:34:57 -0400

    completed lab, passed tests

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
+  function destructivelyAppendCat(Ralph) {
+    if (cats);
+    cats.push('Ralph'); 
+
+  }
+
+  function destructivelyPrependCat(Bob) {
+    cats.unshift('Bob');
+  }
+
+  function destructivelyRemoveLastCat(Milo,Otis) {
+    cats.pop('Garfield');
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+  }
+
+
+  function appendCat(Broom) {
+    return [...cats, Broom];
+ }
